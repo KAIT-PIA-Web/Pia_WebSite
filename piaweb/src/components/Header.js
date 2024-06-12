bb5b658b3e4d0bcfd461e978e8fa7a9ab98913b4
Syachi-o <syachi.ot@gmail.com>
2024-06-12 22:17:41 +0900

    ヘッダー＃１

--- piaweb/src/components/Header.js
+++ piaweb/src/components/Header.js
@@ -2,17 +2,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import PiaLogo from '../asets/images/KaitPiaLogo.png'
+
 
 const Header = () => {
     return (
         <header className="header">
+            <img className="Logo" src={PiaLogo} alt="Piaのロゴ" align="left"></img>
             <nav className="nav">
                 <ul className="nav-list">
                     <li className="nav-item"><Link to="/">Home</Link></li>
-                    <li className="nav-item"><Link to="/about">About</Link></li>
-                    <li className="nav-item"><Link to="/contact">Contact</Link></li>
-                    <li className="nav-item"><Link to="/services">Services</Link></li>
-                    <li className="nav-item"><Link to="/profile">Profile</Link></li>
+                    <li className="nav-item"><Link to="/sosiki">組織構成</Link></li>
+                    <li className="nav-item"><Link to="/katudou">活動報告</Link></li>
+                    <li className="nav-item"><Link to="/piakoe">ピアメンバーの声</Link></li>
+                    <li className="nav-item"><Link to="/QandA">Q&A</Link></li>
                 </ul>
             </nav>
         </header>
